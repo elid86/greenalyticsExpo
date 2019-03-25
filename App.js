f307225d84039ee1c8dec5123e16c120ee08165b
Meaghan Travers <42062453+MegTravers@users.noreply.github.com>
2019-03-25 12:03:34 -0500

    Update App.js

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Sup World!</Text>
+        <Text>Meaghan says hi!!!!</Text>
       </View>
     );
   }
