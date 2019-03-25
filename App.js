7c7e97564124efc14a14703682b0eb08268a77fc
Elizabeth Doyle <Elizabeth.doyle.86@hotmail.com>
2019-03-25 12:13:35 -0500

    Changed text
    "showing meaghan changes"

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Meaghan and Brooke!!!!</Text>
+        <Text>Showing Meaghan changes!</Text>
       </View>
     );
   }
