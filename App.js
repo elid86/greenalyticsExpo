240ae4112319c25dda3dfded4ec0abff0b6f7899
Elizabeth Doyle <Elizabeth.doyle.86@hotmail.com>
2019-03-26 12:44:23 -0500

    added base navigation
    Still very empty.
    MUST ENSURE THE FOLLOWING ARE INSTALLED...
    react native cli:
    npm install -g react-native-cli
    yarn: https://yarnpkg.com/en/docs/install#mac-stable
    react nav:
    yarn add react-navigation

--- App.js
+++ App.js
@@ -1,21 +1,42 @@
-import React from 'react';
+'use strict';
+import React, {Component} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {
+  createStackNavigator,
+  createAppContainer
+} from 'react-navigation';
 
-export default class App extends React.Component {
+//------- All the pages -------//
+
+//-Gardens Page
+class GardensPage extends Component<Props> {
+  static navigationOptions = {
+    title: 'My Gardens',
+  };
   render() {
-    return (
-      <View style={styles.container}>
-        <Text>Showing Meaghan changes!</Text>
-      </View>
-    );
+    return <Text style={styles.description}>Your gardens will appear here!</Text>;
   }
 }
 
+//-------- Navigation --------//
+//Adjust the order of the pages here
+
+const RootStack = createStackNavigator({
+  Home: { screen: GardensPage },
+});
+
+//Javascript 3 requires an appContainer to be made
+const App = createAppContainer(RootStack);
+export default App;
+
+
+//--------- Styles ------------//
+//here is object styles. written similar to CSS
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  description: {
+    fontSize: 18,
+    textAlign: 'center',
+    color: '#656565',
+    marginTop: 65,
   },
 });
