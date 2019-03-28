9a01069c3558802a9d8e15fdef2714d490d3e3b4
Elizabeth Doyle <Elizabeth.doyle.86@hotmail.com>
2019-03-27 19:06:17 -0500

    nicer looking rows on tutorial

--- PlantsPage.js
+++ PlantsPage.js
@@ -12,6 +12,36 @@ import {
     Text,
 } from 'react-native';
 
+//------- creates rows for the table ----------//
+class ListItem extends React.PureComponent {
+    _onPress = () => {
+    this.props.onPressItem(this.props.index);
+}
+
+render() {
+    const item = this.props.item;
+    const price = item.price_formatted.split(' ')[0];
+    return (
+        <TouchableHighlight
+    onPress={this._onPress}
+    underlayColor='#dddddd'>
+        <View>
+        <View style={styles.rowContainer}>
+        <Image style={styles.thumb} source={{ uri: item.img_url }} />
+    <View style={styles.textContainer}>
+        <Text style={styles.price}>{price}</Text>
+        <Text style={styles.title}
+    numberOfLines={1}>{item.title}</Text>
+        </View>
+        </View>
+        <View style={styles.separator}/>
+    </View>
+    </TouchableHighlight>
+);
+}
+}
+
+
 
 //------ plantsPage class -------------//
 type Props = {};
@@ -22,17 +52,18 @@ export default class PlantsPage extends Component<Props> {
 
 _keyExtractor = (item, index) => index.toString();
 
-_renderItem = ({item}) => {
-    return (
-        <TouchableHighlight
-    underlayColor='#dddddd'>
-        <View>
-        <Text>{item.title}</Text>
-        </View>
-        </TouchableHighlight>
+_renderItem = ({item, index}) => (
+<ListItem
+item={item}
+index={index}
+onPressItem={this._onPressItem}
+/>
 );
 
+_onPressItem = (index) => {
+    console.log("Pressed row: "+index);
 };
+
 
 render() {
     const { params } = this.props.navigation.state;
@@ -45,3 +76,33 @@ render() {
 );
 }
 }
+
+
+//--------- Styles ------------//
+const styles = StyleSheet.create({
+    thumb: {
+        width: 80,
+        height: 80,
+        marginRight: 10
+    },
+    textContainer: {
+        flex: 1
+    },
+    separator: {
+        height: 1,
+        backgroundColor: '#dddddd'
+    },
+    price: {
+        fontSize: 25,
+        fontWeight: 'bold',
+        color: '#48BBEC'
+    },
+    title: {
+        fontSize: 20,
+        color: '#656565'
+    },
+    rowContainer: {
+        flexDirection: 'row',
+        padding: 10
+    },
+});
