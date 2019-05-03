25ea6bf45b69695e132fb7985598e571516e46a3
cdman678 <45344395+cdman678@users.noreply.github.com>
2019-05-03 02:06:17 -0500

    Update HardwareStatsPage.js
    finished this

--- HardwareStatsPage.js
+++ HardwareStatsPage.js
@@ -1,21 +1,15 @@
 'use strict';
 
-import React, { Component } from 'react';
-import { VictoryLine, VictoryChart, VictoryTheme, VictoryAxis} from "victory-native";
-import {
-    StyleSheet,
-    View,
-    ScrollView,
-    TouchableHighlight,
-    FlatList,
-    Text,
-    TouchableOpacity,
-    TextInput,
-    Button,
-    Alert
-} from 'react-native';
+import React, {Component} from 'react';
+import {VictoryAxis, VictoryChart, VictoryLine, VictoryTheme} from "victory-native";
+import {ActivityIndicator, ScrollView, StyleSheet, Text, TouchableOpacity, View,} from 'react-native';
 
 const userName = "zlef";
+const MACaddress = "b8:27:eb:0d:73:af";
+
+var light = [];
+var temperature = [];
+var humidity = [];
 
 function convertUTCDateToLocalDate(date) {
     var newDate = new Date(date.getTime()+date.getTimezoneOffset()*60*1000);
@@ -28,33 +22,52 @@ function convertUTCDateToLocalDate(date) {
     return newDate;
 }
 
+function format_json (value_list, type){
 
+    for(var i = 0; i<value_list.length; i++){
+        if(type === "light"){
+            light.push({ time: convertUTCDateToLocalDate(new Date(value_list[i][0] )).toLocaleTimeString(),value: value_list[i][1]});
+        }
+        else if(type === "temperature"){
+            temperature.push({ time: convertUTCDateToLocalDate(new Date(value_list[i][0] )).toLocaleTimeString(),value: value_list[i][1]});
+        }
+        else if(type === "humidity"){
+            humidity.push({ time: convertUTCDateToLocalDate(new Date(value_list[i][0] )).toLocaleTimeString(),value: value_list[i][1]});
+        }
+    }
 
-const temp = [
-    { time: convertUTCDateToLocalDate(new Date(1554007749 )).toLocaleTimeString(),value: 66},
-    { time: convertUTCDateToLocalDate(new Date(1554017971 )).toLocaleTimeString(),value: 67},
-    { time: convertUTCDateToLocalDate(new Date(1554027973 )).toLocaleTimeString(),value: 68},
-    { time: convertUTCDateToLocalDate(new Date(1554037975 )).toLocaleTimeString(),value: 72},
-    { time: convertUTCDateToLocalDate(new Date(1554047983 )).toLocaleTimeString(),value: 64},
-    { time: convertUTCDateToLocalDate(new Date(1554057987 )).toLocaleTimeString(),value: 50},
-    { time: convertUTCDateToLocalDate(new Date(1554067992 )).toLocaleTimeString(),value: 45},
-    { time: convertUTCDateToLocalDate(new Date(1554077994 )).toLocaleTimeString(),value: 50},
-    { time: convertUTCDateToLocalDate(new Date(1554087997 )).toLocaleTimeString(),value: 62},
-    { time: convertUTCDateToLocalDate(new Date(1554097999 )).toLocaleTimeString(),value: 69}
-];
+    console.log(type);
+}
 
-const humidity = [
-    { time: convertUTCDateToLocalDate(new Date(1554007971 )).toLocaleTimeString(),value: 44},
-    { time: convertUTCDateToLocalDate(new Date(1554017973 )).toLocaleTimeString(),value: 45},
-    { time: convertUTCDateToLocalDate(new Date(1554027976 )).toLocaleTimeString(),value: 44},
-    { time: convertUTCDateToLocalDate(new Date(1554037983 )).toLocaleTimeString(),value: 46},
-    { time: convertUTCDateToLocalDate(new Date(1554047988 )).toLocaleTimeString(),value: 43},
-    { time: convertUTCDateToLocalDate(new Date(1554057992 )).toLocaleTimeString(),value: 42},
-    { time: convertUTCDateToLocalDate(new Date(1554067995 )).toLocaleTimeString(),value: 45},
-    { time: convertUTCDateToLocalDate(new Date(1554077997 )).toLocaleTimeString(),value: 44},
-    { time: convertUTCDateToLocalDate(new Date(1554087999 )).toLocaleTimeString(),value: 43},
-    { time: convertUTCDateToLocalDate(new Date(1554098001 )).toLocaleTimeString(),value: 44}
-];
+function getDay (h_id,type){
+
+    var now = Math.floor(Date.now() / 1000);
+
+    var yesterday = now - (60*60*24);
+    var url = 'http://greenalytics.ga:5000/api/hardware/'+h_id+'/'+type+'/'+yesterday;
+    return fetch(url)
+        .then((response) => response.json())
+}
+
+function getWeek(h_id,type) {
+
+    var now = Math.floor(Date.now() / 1000);
+
+    var week = now - (60*60*24*7);
+    var url = 'http://greenalytics.ga:5000/api/hardware/'+h_id+'/'+type+'/'+week;
+    return fetch(url)
+        .then((response) => response.json())
+}
+
+function getMonth(h_id,type){
+
+    var now = Math.floor(Date.now() / 1000);
+
+    var month = now - (60*60*24*7*4);
+    var url = 'http://greenalytics.ga:5000/api/hardware/'+h_id+'/'+type+'/'+month;
+    return fetch(url)
+        .then((response) => response.json())
+}
 
 export default class HardwareStatsPage extends Component<Props> {
     static navigationOptions = {
@@ -62,139 +75,152 @@ export default class HardwareStatsPage extends Component<Props> {
     };
 
     constructor(props){
-        super(props)
+        super(props);
+        this.state = {
+            isLoading: true
+        };
     }
 
-    state = {
-        light:[
-            { time: convertUTCDateToLocalDate(new Date(1554007971)).toLocaleTimeString(),value: 123},
-            { time: convertUTCDateToLocalDate(new Date(1554008974)).toLocaleTimeString(),value: 121},
-            { time: convertUTCDateToLocalDate(new Date(1554009976)).toLocaleTimeString(),value: 119},
-            { time: convertUTCDateToLocalDate(new Date(1554017983)).toLocaleTimeString(),value: 121},
-            { time: convertUTCDateToLocalDate(new Date(1554027988)).toLocaleTimeString(),value: 795},
-            { time: convertUTCDateToLocalDate(new Date(1554037993)).toLocaleTimeString(),value: 117},
-            { time: convertUTCDateToLocalDate(new Date(1554047995)).toLocaleTimeString(),value: 17},
-            { time: convertUTCDateToLocalDate(new Date(1554057997)).toLocaleTimeString(),value: 120},
-            { time: convertUTCDateToLocalDate(new Date(1554067999)).toLocaleTimeString(),value: 800},
-            { time: convertUTCDateToLocalDate(new Date(1554908002)).toLocaleTimeString(),value: 1012}
-        ]
+
+    onPress_day = async () => {
+        //Resetting values
+        light = [];
+        temperature = [];
+        humidity = [];
+
+        this.setState({isLoading: true });
+
+        await getDay(MACaddress,"light").then(
+            json => format_json(json, "light")
+        );
+
+        await getDay(MACaddress,"temperature").then(
+            json => format_json(json, "temperature")
+        );
+
+        await getDay(MACaddress,"humidity").then(
+            json => format_json(json, "humidity")
+        );
+
+        this.setState({isLoading: false });
     };
 
-    onPress_day = () => {
-        this.setState({
-            light:[
-                { time: convertUTCDateToLocalDate(new Date(1554007971)).toLocaleTimeString(),value: 123},
-                { time: convertUTCDateToLocalDate(new Date(1554008974)).toLocaleTimeString(),value: 121},
-                { time: convertUTCDateToLocalDate(new Date(1554009976)).toLocaleTimeString(),value: 119},
-                { time: convertUTCDateToLocalDate(new Date(1554017983)).toLocaleTimeString(),value: 121},
-                { time: convertUTCDateToLocalDate(new Date(1554027988)).toLocaleTimeString(),value: 795},
-                { time: convertUTCDateToLocalDate(new Date(1554037993)).toLocaleTimeString(),value: 117},
-                { time: convertUTCDateToLocalDate(new Date(1554047995)).toLocaleTimeString(),value: 17},
-                { time: convertUTCDateToLocalDate(new Date(1554057997)).toLocaleTimeString(),value: 120},
-                { time: convertUTCDateToLocalDate(new Date(1554067999)).toLocaleTimeString(),value: 800},
-                { time: convertUTCDateToLocalDate(new Date(1554908002)).toLocaleTimeString(),value: 1012}
-            ]
-        })
+    onPress_week = async () => {
+        //Resetting values
+        light = [];
+        temperature = [];
+        humidity = [];
+
+        this.setState({isLoading: true });
+
+        await getWeek(MACaddress,"light").then(
+            json => format_json(json, "light")
+        );
+        await getWeek(MACaddress,"temperature").then(
+            json => format_json(json, "temperature")
+        );
+        await getWeek(MACaddress,"humidity").then(
+            json => format_json(json, "humidity")
+        );
+
+        this.setState({isLoading: false });
     };
 
-    onPress_week = () => {
-        this.setState({
-            light:[
-                { time: convertUTCDateToLocalDate(new Date(1554007971)).toLocaleTimeString(),value: 988},
-                { time: convertUTCDateToLocalDate(new Date(1554008974)).toLocaleTimeString(),value: 225},
-                { time: convertUTCDateToLocalDate(new Date(1554009976)).toLocaleTimeString(),value: 42},
-                { time: convertUTCDateToLocalDate(new Date(1554017983)).toLocaleTimeString(),value: 988},
-                { time: convertUTCDateToLocalDate(new Date(1554027988)).toLocaleTimeString(),value: 1012},
-                { time: convertUTCDateToLocalDate(new Date(1554037993)).toLocaleTimeString(),value: 15},
-                { time: convertUTCDateToLocalDate(new Date(1554047995)).toLocaleTimeString(),value: 600},
-                { time: convertUTCDateToLocalDate(new Date(1554057997)).toLocaleTimeString(),value: 420},
-                { time: convertUTCDateToLocalDate(new Date(1554067999)).toLocaleTimeString(),value: 69},
-                { time: convertUTCDateToLocalDate(new Date(1554908002)).toLocaleTimeString(),value: 69}
-            ]
-        })
-    };
+    onPress_month = async () => {
+        //Resetting values
+        light = [];
+        temperature = [];
+        humidity = [];
 
-    onPress_month = () => {
-        this.setState({
-            light:[
-                { time: convertUTCDateToLocalDate(new Date(1554007971)).toLocaleTimeString(),value: 400},
-                { time: convertUTCDateToLocalDate(new Date(1554008974)).toLocaleTimeString(),value: 500},
-                { time: convertUTCDateToLocalDate(new Date(1554009976)).toLocaleTimeString(),value: 600},
-                { time: convertUTCDateToLocalDate(new Date(1554017983)).toLocaleTimeString(),value: 200},
-                { time: convertUTCDateToLocalDate(new Date(1554027988)).toLocaleTimeString(),value: 600},
-                { time: convertUTCDateToLocalDate(new Date(1554037993)).toLocaleTimeString(),value: 500},
-                { time: convertUTCDateToLocalDate(new Date(1554047995)).toLocaleTimeString(),value: 200},
-                { time: convertUTCDateToLocalDate(new Date(1554057997)).toLocaleTimeString(),value: 400},
-                { time: convertUTCDateToLocalDate(new Date(1554067999)).toLocaleTimeString(),value: 900},
-                { time: convertUTCDateToLocalDate(new Date(1554908002)).toLocaleTimeString(),value: 200}
-            ]
-        })
+        this.setState({isLoading: true });
+
+        await getMonth(MACaddress,"light").then(
+            json => format_json(json, "light")
+        );
+        await getMonth(MACaddress,"temperature").then(
+            json => format_json(json, "temperature")
+        );
+        await getMonth(MACaddress,"humidity").then(
+            json => format_json(json, "humidity")
+        );
+
+        this.setState({isLoading: false });
     };
 
 
 
 //--load existing Beds
-    componentDidMount(){
+    async componentDidMount() {
         //******* USE THIS TO PASS THE HARDWARE YOU'RE LOOKING AT AND CALL THE FUNCTION THAT CALLS THE API
-        const {params} = this.props.navigation.state;
+        //const {params} = this.props.navigation.state;
+
+        await this.onPress_day()
+        // console.log(temperature);
+        // console.log(light);
+        this.setState({isLoading: false })
     }
 
-
-
     render() {
-
-        return (
-            <ScrollView>
+        if (this.state.isLoading) {
+            return (
+                < View style={{top: 100}}>
+                    < ActivityIndicator size='large'/>
+                < /View>
+            );
+        }
+        else{
+                return (
+                <ScrollView>
 
 
                 <View style={styles.container1}>
-                    <TouchableOpacity onPress={this.onPress_day} style={styles.fab1}>
-                        <Text style={styles.fabIcon}>Day</Text>
-                    </TouchableOpacity>
+                <TouchableOpacity onPress={this.onPress_day} style={styles.fab1}>
+                <Text style={styles.fabIcon}>Day</Text>
+                </TouchableOpacity>
 
-                    <TouchableOpacity onPress={this.onPress_week} style={styles.fab2}>
-                        <Text style={styles.fabIcon}>Week</Text>
-                    </TouchableOpacity>
+                <TouchableOpacity onPress={this.onPress_week} style={styles.fab2}>
+                <Text style={styles.fabIcon}>Week</Text>
+                </TouchableOpacity>
 
-                    <TouchableOpacity onPress={this.onPress_month} style={styles.fab3}>
-                        <Text style={styles.fabIcon}>Month</Text>
-                    </TouchableOpacity>
+                <TouchableOpacity onPress={this.onPress_month} style={styles.fab3}>
+                <Text style={styles.fabIcon}>Month</Text>
+                </TouchableOpacity>
 
                 </View>
 
-                <View style={styles.container2}>
+                <View style={styles.container2} pointerEvents="none">
+                <Text>Sunlight</Text>
+                <VictoryChart
+                width={350}
+                theme={VictoryTheme.material}>
 
-                    <Text>Sunlight</Text>
-                    <VictoryChart
-                        width={350}
-                        theme={VictoryTheme.material}
-                        animate={{ onLoad: { duration: 800 } }}>
-                        <VictoryLine data={this.state.light} x="time" y="value"/>
-                        <VictoryAxis fixLabelOverlap={true}/>
-                        <VictoryAxis dependentAxis/>
-                    </VictoryChart>
-                    <Text>Temperature</Text>
-                    <VictoryChart
-                        width={350}
-                        theme={VictoryTheme.material}
-                        animate={{ onLoad: { duration: 800 } }}>
-                        <VictoryLine data={temp} x="time" y="value"/>
-                        <VictoryAxis fixLabelOverlap={true}/>
-                        <VictoryAxis dependentAxis/>
-                    </VictoryChart>
-                    <Text>Humidity</Text>
-                    <VictoryChart
-                        width={350}
-                        theme={VictoryTheme.material}
-                        animate={{ onLoad: { duration: 800 } }}>
-                        <VictoryLine data={humidity} x="time" y="value"/>
-                        <VictoryAxis fixLabelOverlap={true}/>
-                        <VictoryAxis dependentAxis/>
-                    </VictoryChart>
+                <VictoryLine data={light} x="time" y="value"/>
+                <VictoryAxis fixLabelOverlap={true}/>
+                <VictoryAxis dependentAxis/>
+                </VictoryChart>
+                <Text>Temperature</Text>
+                <VictoryChart
+                width={350}
+                theme={VictoryTheme.material}>
+
+                <VictoryLine data={temperature} x="time" y="value"/>
+                <VictoryAxis fixLabelOverlap={true}/>
+                <VictoryAxis dependentAxis/>
+                </VictoryChart>
+                <Text>Humidity</Text>
+                <VictoryChart
+                width={350}
+                theme={VictoryTheme.material}>
+
+                <VictoryLine data={humidity} x="time" y="value"/>
+                <VictoryAxis fixLabelOverlap={true}/>
+                <VictoryAxis dependentAxis/>
+                </VictoryChart>
                 </View>
 
-            </ScrollView>
-        );
+                </ScrollView>
+                );
+            }
     }
 
 
